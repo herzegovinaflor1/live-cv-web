0f5c533777930a09e3547ce8063c4d623ad92737
Dmytro Pyvovarenko <dmitriy.pivovarenko@gmail.com>
2022-04-24 10:34:58 +0200

    Adjust Heroku env

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hello-angular4'));
+app.use(express.static('./dist/hello-angular4'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/hello-angular4/src/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/hello-angular4/' })
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,23 @@ import { CvContainerComponent } from './pages/cv-container/cv-container.componen
 import { LoginComponent } from './pages/login/login.component';
 import { ShareProfileComponent } from './pages/share-profile/share-profile.component';
 
- 
+
 export const routes: Routes = [
-  { 
-    path: 'cv/:user_id', 
-    component: CvContainerComponent,
-    canActivate: []
+  {
+    path: 'cv/:user_id',
+    component: CvContainerComponent
   },
-  { 
-    path: 'share', 
-    component: ShareProfileComponent,
-    canActivate: []
+  {
+    path: 'share',
+    component: ShareProfileComponent
   },
-  { path: '', component: LoginComponent },
+  { path: '', component: LoginComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { 
+export class AppRoutingModule {
 
 }
